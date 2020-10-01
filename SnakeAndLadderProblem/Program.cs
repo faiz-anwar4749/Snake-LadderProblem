9dae669a28e6a69b508488d457e749be7f18fb23
Faiz <faiz.anwar4749@gmail.com>
2020-10-01 07:59:15 +0530

    [Faiz] Refactor: Repeating till Player reaches Winning Position

--- SnakeAndLadderProblem/Program.cs
+++ SnakeAndLadderProblem/Program.cs
@@ -7,14 +7,12 @@ namespace SnakeAndLadderProblem
         public const int NoPlay = 0;
         public const int Ladder = 1;
         public const int Snake = 2;
-
         public static int RollTheDie()
         {
             Random random = new Random();
             int numOnDie = random.Next(1, 7);
             return numOnDie;
         }
-
         public static int newPosition(int numOnDie)
         {
             int toAdd = 0;
@@ -23,27 +21,35 @@ namespace SnakeAndLadderProblem
             switch (toMove)
             {
                 case NoPlay:
-                    Console.WriteLine("No Play");
                     break;
                 case Ladder:
                     toAdd = numOnDie;
-                    Console.WriteLine("Ladder, moving forwards.");
                     break;
                 case Snake:
                     toAdd = -numOnDie;
-                    Console.WriteLine("Snake, moving backwards");
                     break;
             }
             return toAdd;
-
-            static void Main(string[] args)
+        }
+        static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Snake and Ladder Simulator");
-            int initialPosition = 0;
-            Console.WriteLine("Starting position = " + initialPosition);
-            int numOnDie = RollTheDie();
-            Console.WriteLine("Number on the Die rolled = " + numOnDie);
-            Console.WriteLine("New position = " + newPosition(numOnDie));
+            int initialPosition = 0, nextPosition;
+            int count = 0;
+            while (initialPosition < 100)
+            {
+                int numOnDie = RollTheDie();
+                count++;
+                int toAdd = newPosition(numOnDie);
+                nextPosition = initialPosition + toAdd;
+                //checking if player has moved below position 0
+                if (nextPosition < 0)
+                    initialPosition = 0;
+                else
+                    initialPosition = nextPosition;
             }
+            Console.WriteLine("Number on the Die rolled = " + count);
+            Console.WriteLine("Current position = " + initialPosition);
+        }
     }
 }
