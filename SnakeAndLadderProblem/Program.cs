dc2eb27a7d9426768907262915d40748b935a521
Faiz <faiz.anwar4749@gmail.com>
2020-10-01 07:28:20 +0530

    [Faiz] Refactor : Rolling the Die and displaying the number

--- SnakeAndLadderProblem/Program.cs
+++ SnakeAndLadderProblem/Program.cs
@@ -4,11 +4,22 @@ namespace SnakeAndLadderProblem
 {
     class Program
     {
+        public static int RollTheDie()
+        {
+            Random random = new Random();
+            int numOnDie = random.Next(1, 7);
+            return numOnDie;
+        }
+
+
+
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Snake and Ladder Simulator");
             int initialPosition = 0;
             Console.WriteLine("Starting position = " + initialPosition);
+            int numOnDie = RollTheDie();
+            Console.WriteLine("Number on the Die rolled = " + numOnDie);
         }
     }
 }
