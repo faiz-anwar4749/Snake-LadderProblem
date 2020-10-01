8df4f482dbb4f7be86a3fac5b75b73c2f13dc057
Faiz <faiz.anwar4749@gmail.com>
2020-10-01 07:38:27 +0530

    [Faiz] Refactor : Checking the option that player gets

--- SnakeAndLadderProblem/Program.cs
+++ SnakeAndLadderProblem/Program.cs
@@ -4,6 +4,10 @@ namespace SnakeAndLadderProblem
 {
     class Program
     {
+        public const int NoPlay = 0;
+        public const int Ladder = 1;
+        public const int Snake = 2;
+
         public static int RollTheDie()
         {
             Random random = new Random();
@@ -11,15 +15,35 @@ namespace SnakeAndLadderProblem
             return numOnDie;
         }
 
+        public static int newPosition(int numOnDie)
+        {
+            int toAdd = 0;
+            Random random = new Random();
+            int toMove = random.Next(0, 3);
+            switch (toMove)
+            {
+                case NoPlay:
+                    Console.WriteLine("No Play");
+                    break;
+                case Ladder:
+                    toAdd = numOnDie;
+                    Console.WriteLine("Ladder, moving forwards.");
+                    break;
+                case Snake:
+                    toAdd = -numOnDie;
+                    Console.WriteLine("Snake, moving backwards");
+                    break;
+            }
+            return toAdd;
 
-
-        static void Main(string[] args)
+            static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Snake and Ladder Simulator");
             int initialPosition = 0;
             Console.WriteLine("Starting position = " + initialPosition);
             int numOnDie = RollTheDie();
             Console.WriteLine("Number on the Die rolled = " + numOnDie);
-        }
+            Console.WriteLine("New position = " + newPosition(numOnDie));
+            }
     }
 }
